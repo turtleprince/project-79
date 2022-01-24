a0c6e8ffb76b6723ca850c80daab2938399018cd
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-01-23 19:14:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ menu_list_array = ["Veg Margherita Pizza",
 		var imgtags='<img id="im1" src="pizzaImg.png">'
         var item=document.getElementById("add_item").value;
         // use the sort function as - menu_list_array.sort();
-        htmldata="menu_list_array.sort();";
+        menu_list_array.sort();
+        htmldata ="";
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
         }
